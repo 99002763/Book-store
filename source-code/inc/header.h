012d0d53ea0644d57dde3860ae83c3041b8c72a5
99002763 <72455156+99002763@users.noreply.github.com>
2020-10-07 00:03:30 +0530

    Update header.h

--- source-code/inc/header.h
+++ source-code/inc/header.h
@@ -1,1 +1,18 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
 
+#include <stdio.h> 
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+
+void showMenu();
+void addBook();
+void showAllBooks();
+char getAnswer();
+void showCheckMenu();
+void setCategory();
+void eraseBooks();
+
+#endif
